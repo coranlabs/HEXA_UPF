3eaa90327246fa7a3f6fc6f17f6c8118961ad95b
Shiva-Marshall <Shiva-Marshall@users.noreply.github.com>
2024-08-15 10:19:40 +0530

    ADD: Implementation of UDP server for listening incoming packets
    Signed-off-by: Shiva-Marshall <Shiva-Marshall@users.noreply.github.com>

--- src/internal/pfcpconn.go
+++ src/internal/pfcpconn.go
@@ -3,8 +3,46 @@
 
 package internal
 
+import (
+	"log"
+	"net"
+	"time"
+)
+
+func CreateConn(addr string, nodeid string, n3Ip string) (*PfcpConn, error) {
+	udpAddr := "192.168.1.2"
+	if udpAddr != nil {
+		return nil, nil
+	}
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return nil, err
+	}
+	return &PfcpConn{
+		udpConn:           udpConn,
+		nodeId:            nodeid,
+		nodeAddrV4:        udpAddr.IP,
+		RecoveryTimestamp: time.Now(),
+	}, nil
+}
+
 func (connection *PfcpConn) Run() {
-	println("Starting the Server")
+	log.Printf("Starting the Server")
+	buf := make([]byte, 1500)
+	log.Printf("Server Started")
+	for {
+		n, addr, err := connection.Receive(buf)
+		if err != nil {
+			log.Printf("Error reading from UDP socket: %s", err.Error())
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		log.Debugf("Received %d bytes from %s", n, addr)
+	}
+}
+
+func (connection *PfcpConn) Receive(b []byte) (n int, addr *net.UDPAddr, err error) {
+	return connection.udpConn.ReadFromUDP(b)
 }
 
 func (connection *PfcpConn) Close() {}

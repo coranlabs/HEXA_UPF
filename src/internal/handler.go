f09cff7a8afbb2edf9db3340dfc97fa955ccd49a
kcl17 <khushichhillar02@gmail.com>
2024-08-17 12:12:07 +0530

    UPDATE: defined basic pfcp message handling in handler.go and updated logger
    Signed-off-by: kcl17 <khushichhillar02@gmail.com>

--- src/internal/handler.go
+++ src/internal/handler.go
@@ -1,7 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 CORAN LABS
+
 package internal
 
-import "net"
+import (
+	"log"
+	"net"
 
-func Handle(conn *PfcpConn, buf []byte, addr *net.UDPAddr) error{
+	mes "github.com/wmnsk/go-pfcp/message"
+)
+
+func Handle(conn *PfcpConn, buf []byte, addr *net.UDPAddr) error {
+	msg, err := mes.Parse(buf)
+	if err != nil {
+
+		return err
+	}
+
+	for {
+		switch msg.MessageType() {
+		case mes.MsgTypeAssociationSetupRequest:
+			HandlePfcpAssociationSetupRequest(conn, msg)
+		default:
+			log.Printf("Got unexpected mes %s: %s, from: %s", msg.MessageTypeName(), msg, addr)
+			return nil
+		}
+	}
+}
+
+func HandlePfcpAssociationSetupRequest(conn *PfcpConn, msg mes.Message) {}
+
+func NewNodeAssociation(remoteNodeID string, addr string) *NodeAssociation {
+	return &NodeAssociation{}
+}
+
+func (association *NodeAssociation) ScheduleHeartbeat(conn *PfcpConn) {}
+
+func (connection *PfcpConn) SendMessage(msg mes.Message, addr *net.UDPAddr) error {
 	return nil
 }

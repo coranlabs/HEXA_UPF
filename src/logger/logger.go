8ebb6a749c3d44380df2c7cd10c0fcc6c5e5a7fe
Shiva-Marshall <Shiva-Marshall@users.noreply.github.com>
2024-08-17 12:12:07 +0530

    UPDATE: defined basic pfcp message handling in handler.go and updated logger
    Signed-off-by: Shiva-Marshall <Shiva-Marshall@users.noreply.github.com>

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 CORAN LABS
+
 package logger
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func init() {
+	log = logrus.New()
+	log.SetReportCaller(false)
+}
